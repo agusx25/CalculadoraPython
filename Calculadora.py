8be4c58742e71f26271956c78e2b84c413957124
agusx25 <108071935+agusx25@users.noreply.github.com>
2022-06-24 11:16:35 -0300

    último ttk

--- Calculadora.py
+++ Calculadora.py
@@ -6,50 +6,34 @@ import random
 
 class Calculadora():
     def __init__(self):
-        # def changeText(numero):
 
-        #     text.set(str(numero))
-        # gui = tk.Tk()
-        # gui.geometry('300x100')
-        #resultado = ttk.StringVar()
-        # text.set('Hola pete')
-
-        # label = tk.Label(gui, textvariable=text)
-        # label.pack(pady=50)
-
-        # button = tk.Button(gui, text='hola pete', command= lambda :  changeText(random.randint(0,100)))
-        # button.pack()
-        # gui.mainloop()
         root = tk.Tk()
+        root.columnconfigure(0, weight=1)
+        root.columnconfigure(1, weight=3)
         self.labelResultado = tk.StringVar()
-        self.labelResultado.set('0')
         self.frm = ttk.Frame(root, padding=10)
         self.frm.grid()
-        self.ttkLabel = ttk.Label(self.frm, textvariable=self.labelResultado).grid(column=4, row=0)
+        self.ttkLabel = ttk.Entry(self.frm, textvariable=self.labelResultado, width=40)
+        self.ttkLabel.grid(column=0, row=0, columnspan=4, sticky="nsew")
         self.operators = ['+', '-', '*', '/']
         # generar botones
-        for i in range(1, 10):
-            column = i-3 * ((i-1)//3)
-            row = (i+2)//3
-            ttk.Button(self.frm, text=i, command=lambda i=i: self.add_numbers(str(i))).grid(column=column, row=row) 
 
-            
         for i in range(len(self.operators)):
-            ttk.Button(self.frm, text=self.operators[i], command=lambda i=i: self.add_numbers(self.operators[i])).grid(column=4, row=i+1)
-        ttk.Button(self.frm, text='=', command=self.solve_equation).grid( column=4, row=5)
+            ttk.Button(self.frm, text=self.operators[i], command=lambda i=i: self.add_numbers(self.operators[i])).grid(column=i, row=1,  pady = 20, sticky="nsew")
+        b= ttk.Button(self.frm, text='=', command=self.solve_equation)
+        b.grid( column=4, row=1, rowspan=2) 
         root.mainloop()
         
     def add_numbers(self, number):
         resultado = self.labelResultado.get()
-        if resultado == '0':
-            self.labelResultado.set(number)
-        else:
-            self.labelResultado.set(resultado + number)
+
+        self.labelResultado.set(resultado + number)
+        self.ttkLabel.focus_set()
+        self.ttkLabel.icursor(len(resultado)+1)
 
     def solve_equation(self):
         try:
             self.labelResultado.set(eval(self.labelResultado.get()))
-
         except SyntaxError:
             self.labelResultado.set("Error de sintaxis")
         except ZeroDivisionError:
