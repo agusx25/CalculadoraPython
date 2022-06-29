7b778fbd4bf6da93c183f90d01c4508fabc39b13
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2022-06-29 11:41:51 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,43 +1,27 @@
-import tkinter as tk
-
-from tkinter import ttk
-import random
-
-
-class Calculadora():
+class EqSolver():
     def __init__(self):
+        self.terminos = []
+        self.operations = []
+    def resolve(self, equation):
+        self.terminos = equation.replace('-',' -').replace('+',' ').split()
+        for i in equation:
+            if i == '+' or i == '-':
+                self.operations.append(i)
+    def multiply(self, equation):
+        for termino_index in range(len(self.terminos)):
+            if '*' in self.terminos[termino_index]:
+                pass
+                
 
-        root = tk.Tk()
-        root.columnconfigure(0, weight=1)
-        root.columnconfigure(1, weight=3)
-        self.labelResultado = tk.StringVar()
-        self.frm = ttk.Frame(root, padding=10)
-        self.frm.grid()
-        self.ttkLabel = ttk.Entry(self.frm, textvariable=self.labelResultado, width=40)
-        self.ttkLabel.grid(column=0, row=0, columnspan=4, sticky="nsew")
-        self.operators = ['+', '-', '*', '/']
-        # generar botones
+if __name__ == '__main__':
+    eqsol = EqSolver()
+    print("hola")
+# def resolve(equation):
+#     for i in equation:
+#     separarN = self.terminos[i].replace('*',' ').replace('/',' ')
+#           for i in equation:
+#             if i == '*' or i == '/':
+#                 separarO.append(i)
+# "1*2*3*4*5/6/6*4/3"
 
-        for i in range(len(self.operators)):
-            ttk.Button(self.frm, text=self.operators[i], command=lambda i=i: self.add_numbers(self.operators[i])).grid(column=i, row=1,  pady = 20, sticky="nsew")
-        b= ttk.Button(self.frm, text='=', command=self.solve_equation)
-        b.grid( column=4, row=1, rowspan=2) 
-        root.mainloop()
-        
-    def add_numbers(self, number):
-        resultado = self.labelResultado.get()
-
-        self.labelResultado.set(resultado + number)
-        self.ttkLabel.focus_set()
-        self.ttkLabel.icursor(len(resultado)+1)
-
-    def solve_equation(self):
-        try:
-            self.labelResultado.set(eval(self.labelResultado.get()))
-        except SyntaxError:
-            self.labelResultado.set("Error de sintaxis")
-        except ZeroDivisionError:
-            pass
-
-
-calculadora = Calculadora()
+# 6/2(1+2)
